00b2144e3732a6c9a8b800842fbaae680e3253cc
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2024-01-15 18:57:35 +0100

    FirstCommit
    - Creating SpringMongo Project

---
+++ src/main/java/com/bank/MVC_AOM_MR/MvcAomMrApplication.java
@@ -0,0 +1,13 @@
+package com.bank.MVC_AOM_MR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcAomMrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcAomMrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bank/MVC_AOM_MR/MvcAomMrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bank.MVC_AOM_MR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcAomMrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
