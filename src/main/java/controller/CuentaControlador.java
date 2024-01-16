0498b13a6e4c4a6e60bf59ea222be295a36f5260
Alejandro Ortega Maldonado <mountimeproject@gmail.com>
2024-01-16 20:58:15 +0100

    16/01/2024
    Se hicieron los siguientes cambios en las clases descritas a continuación:
    
    - CuentaControlador.java : Se añadió la estructura de algunos métodos GET, PUT y DELETE

--- src/main/java/controller/CuentaControlador.java
+++ src/main/java/controller/CuentaControlador.java
@@ -1,11 +1,137 @@
 package controller;
 
-public class CuentaControlador {
-
 // controlador --> Objeto: cuentaControlador.java
 //			- Rutas de las api
 //			- Get...Put..
 //	. Mandan Objetos entre controlador y modelo 
-//	
-//
+import java.util.ArrayList;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class CuentaControlador {
+
+	// Mostrar todas las cuentas bancarias: /banco/cuenta (GET) (No debe mostrar las cuentas borradas):
+	
+	@GetMapping("/banco/cuenta/")
+	public int getCuentas(@PathVariable int numero1, @PathVariable int numero2) {
+		return numero1+numero2;
+		
+	}
+	
+	// Mostrar cuentas bancarias por número de cuenta: /banco/cuenta/{nro_cuenta} (GET):
+	
+
+    @GetMapping("/banco/cuenta/{nro_cuenta}")
+    public int getCuentasNum(@PathVariable int numero1, @PathVariable int numero2) {
+        return  numero1 * numero2;
+    }
+    
+    // Mostrar cuentas bancarias por rango de fecha de apertura: /banco/cuenta/{fecha_ini}/{fecha_fin} (GET):
+    
+     @GetMapping("/banco/cuenta/{fecha_ini}/{fecha_fin}")
+     public boolean mayorEdad(@PathVariable int edad) {
+    	 boolean mayorEdadBool;
+    	 if (edad < 18) {
+    		 mayorEdadBool= false;
+    	 }else {
+    		 
+             mayorEdadBool = true;
+     }
+    	 return mayorEdadBool;
+     }
+     
+     
+    // Insertar nuevas cuentas bancarias: /banco/cuenta/new (PUT):
+         @PutMapping("/banco/cuenta/new")
+         public boolean crearCuenta(@PathVariable String nombre) {
+			boolean estado = false;
+        	 return false;
+         }
+     
+    // Actualizar cuentas bancarias por número de cuenta: /banco/cuenta/update (PUT):
+         @GetMapping("/banco/cuenta/update")
+     	public ArrayList<Integer> listaNumeros(@PathVariable int n) {
+        	 ArrayList<Integer> lista = new ArrayList<Integer>();
+        	 for (int i = 1; i <= n; i++) {
+        		lista.add(i);
+			}
+			return lista;
+        	
+     		
+     	}
+         
+         
+         @PostMapping("/sumaPost")
+     	public int sumaPost(@RequestParam int numero1, @RequestParam int numero2) {
+     		return numero1+numero2;
+     	}
+         
+         // Borrar cuentas bancarias por numero de cuenta: /banco/cuenta/{nro_cuenta} (DELETE) (soft deletion):
+         @DeleteMapping("/banco/cuenta/{numeroDeCuenta}")
+         public boolean borraCuentaBancaria(@RequestParam String numeroDeCuenta) {
+        	 boolean mayorEdadBool=true;
+        	
+        	 return mayorEdadBool;
+         }
+
+         
+         // Actualizar cuentas bancarias por número de cuenta: /banco/cuenta/update (PUT):
+         @PutMapping("/banco/cuenta/update")
+         public boolean mayorEdadPut(@RequestParam int edad) {
+        	 boolean mayorEdadBool;
+        	 if (edad < 18) {
+        		 mayorEdadBool= false;
+        	 }else {
+        		 
+                 mayorEdadBool = true;
+         }
+        	 return mayorEdadBool;
+         }
+
+
+//         Insertar dinero por numero de cuenta: /banco/cuenta/ingresar/ (PUT con datos {nro_cuenta} e {ingreso})
+//         Devuelve un string con el saldo ingresado y el saldo restante.
+//         Debe actualizar el saldo.
+//         Si el dinero a ingresar es negativo, no hace nada.
+         
+         @PutMapping("/banco/cuenta/ingresar/")
+         public boolean ingresarDinero(@RequestParam int edad) {
+        	 boolean mayorEdadBool;
+        	 if (edad < 18) {
+        		 mayorEdadBool= false;
+        	 }else {
+        		 
+                 mayorEdadBool = true;
+         }
+        	 return mayorEdadBool;
+         }
+         
+//         Retirar dinero por numero de cuenta: /banco/cuentas/retirar/ (PUT con datos {nro_cuenta} y {retiro})
+//         Devuelve un string con el saldo retirado y el saldo restante.
+//         Debe actualizar el saldo.
+//         Si el dinero a retirar es negativo no hace nada.
+//         Si el saldo de la cuenta se queda a 0, solo debe retirar el saldo restante.
+         
+         @PutMapping("/banco/cuenta/retirar/")
+         public boolean retirarDinero(@RequestParam int edad) {
+        	 boolean mayorEdadBool;
+        	 if (edad < 18) {
+        		 mayorEdadBool= false;
+        	 }else {
+        		 
+                 mayorEdadBool = true;
+         }
+        	 return mayorEdadBool;
+         }
 }
+//
+
--- src/main/java/model/CuentaBancaria.java
+++ src/main/java/model/CuentaBancaria.java
@@ -5,74 +5,68 @@ import java.util.Date;
 import java.util.List;
 
 public class CuentaBancaria {
-    private String numeroDeCuenta;
-    private List<String> titulares;
-    private double saldo;
-    private Date fechaApertura;
-    private boolean borrada;
-//	
-//- Modelo --> CuentaBancaria.java
-//		- Clase del objeto
+	private String numero_de_cuenta;
+	private List<String> titulares;
+	private double saldo;
+	private Date fecha_apertura;
+	private boolean borrada;
 
-
-	    // Constructor por defecto
-	    public CuentaBancaria() {
-	        this.titulares = new ArrayList<>();
-	        this.fechaApertura = new Date();
-	        this.borrada = false;
-	    }
-
-	    // Constructor con parámetros
-	    public CuentaBancaria(String numeroDeCuenta, List<String> titulares, double saldo, Date fechaApertura, boolean borrada) {
-	        this.numeroDeCuenta = numeroDeCuenta;
-	        this.titulares = titulares;
-	        this.saldo = saldo;
-	        this.fechaApertura = fechaApertura;
-	        this.borrada = borrada;
-	    }
-
-	    // Getters y setters para cada atributo
-
-	    public String getNumeroDeCuenta() {
-	        return numeroDeCuenta;
-	    }
-
-	    public void setNumeroDeCuenta(String numeroDeCuenta) {
-	        this.numeroDeCuenta = numeroDeCuenta;
-	    }
-
-	    public List<String> getTitulares() {
-	        return titulares;
-	    }
-
-	    public void setTitulares(List<String> titulares) {
-	        this.titulares = titulares;
-	    }
-
-	    public double getSaldo() {
-	        return saldo;
-	    }
-
-	    public void setSaldo(double saldo) {
-	        this.saldo = saldo;
-	    }
-
-	    public Date getFechaApertura() {
-	        return fechaApertura;
-	    }
-
-	    public void setFechaApertura(Date fechaApertura) {
-	        this.fechaApertura = fechaApertura;
-	    }
-
-	    public boolean isBorrada() {
-	        return borrada;
-	    }
-
-	    public void setBorrada(boolean borrada) {
-	        this.borrada = borrada;
-	    }
+	// Constructor por defecto
+	public CuentaBancaria() {
+		this.titulares = new ArrayList<>();
+		this.fecha_apertura = new Date();
+		this.borrada = false;
 	}
 
-	
+	// Constructor con parámetros
+	public CuentaBancaria(String numeroDeCuenta, List<String> titulares, double saldo, Date fechaApertura,
+			boolean borrada) {
+		this.numero_de_cuenta = numeroDeCuenta;
+		this.titulares = titulares;
+		this.saldo = saldo;
+		this.fecha_apertura = fechaApertura;
+		this.borrada = borrada;
+	}
 
+	// Getters y setters para cada atributo
+
+	public String getNumeroDeCuenta() {
+		return numero_de_cuenta;
+	}
+
+	public void setNumeroDeCuenta(String numeroDeCuenta) {
+		this.numero_de_cuenta = numeroDeCuenta;
+	}
+
+	public List<String> getTitulares() {
+		return titulares;
+	}
+
+	public void setTitulares(List<String> titulares) {
+		this.titulares = titulares;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public Date getFechaApertura() {
+		return fecha_apertura;
+	}
+
+	public void setFechaApertura(Date fechaApertura) {
+		this.fecha_apertura = fechaApertura;
+	}
+
+	public boolean isBorrada() {
+		return borrada;
+	}
+
+	public void setBorrada(boolean borrada) {
+		this.borrada = borrada;
+	}
+}
